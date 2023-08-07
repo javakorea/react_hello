4ccbc4e26ec9e1da3824844e59eeb1b0ba10f82c
thehack <thehack@naver.com>
2023-08-07 20:48:37 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-//
-const express = require("express");
-const path = require("path");
+function App() {
+  return (
+    <div>Hello teammoca!</div>
+  );
+}
 
-const app = express();
-
-app.set("port", process.env.PORT || 5000);
-
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
-
-app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기중..");
-});
-//
-// function App() {
-//   return (
-//     <div>Hello teammoca!</div>
-//   );
-// }
-
-export default app;
+export default App;
